bb33c172a96e8484b16402ebf87753595ae5117f
ratkosrb <ratkomladic2@abv.bg>
2018-09-27 06:36:50 +0300

    Remove duplicate scripts.

--- src/server/scripts/EasternKingdoms/eastern_kingdoms_script_loader.cpp
+++ src/server/scripts/EasternKingdoms/eastern_kingdoms_script_loader.cpp
@@ -16,11 +16,6 @@
  */
 
 // This is where scripts' loading functions should be declared:
-void AddSC_alterac_valley();                 //Alterac Valley
-void AddSC_boss_balinda();
-void AddSC_boss_drekthar();
-void AddSC_boss_galvangar();
-void AddSC_boss_vanndar();
 void AddSC_blackrock_depths();               //Blackrock Depths
 void AddSC_boss_ambassador_flamelash();
 void AddSC_boss_draganthaurissan();
@@ -193,11 +188,6 @@ void AddSC_western_plaguelands();
 // void Add${NameOfDirectory}Scripts()
 void AddEasternKingdomsScripts()
 {
-    AddSC_alterac_valley();                 //Alterac Valley
-    AddSC_boss_balinda();
-    AddSC_boss_drekthar();
-    AddSC_boss_galvangar();
-    AddSC_boss_vanndar();
     AddSC_blackrock_depths();               //Blackrock Depths
     AddSC_boss_ambassador_flamelash();
     AddSC_boss_draganthaurissan();
